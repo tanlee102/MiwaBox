9c66517c17663e74af0618766791addab1e879d0
LE QUOC TAN <letanprox@gmail.com>
2024-04-14 20:44:19 +0700

    I

--- app/env_video.js
+++ app/env_video.js
@@ -1,3 +1,3 @@
-export const LENGTH_LIST_VIDEO = 15;
+export const LENGTH_LIST_VIDEO = 21;
 export const url_upload = 'https://thumb.miwabox.live/api';
 export const url_image_domain = 'https://thumb.miwabox.live/';
