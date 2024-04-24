a4d3e2a67bf8c447884db97e36f7c63bad4df6ff
LE QUOC TAN <letanprox@gmail.com>
2024-04-24 23:28:37 +0700

    P

--- app/Components/OneDrivePlayer.js
+++ app/Components/OneDrivePlayer.js
@@ -29,7 +29,7 @@ const OneDrivePlayer = ({src}) => {
             <div className="loader-hozon"></div>
         </div>
       </div>
-      : <video className='video-onedrive-player' src={videoSrc} controls></video>
+      : <video crossorigin="anonymous" className='video-onedrive-player' src={videoSrc} controls></video>
   )
 }
 
