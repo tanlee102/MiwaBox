501b3fd4b558b6b6541807dde5fc7556e59540f6
LE QUOC TAN <letanprox@gmail.com>
2024-05-05 11:46:07 +0700

    P

--- OutRun/run_data_suggestion.js
+++ OutRun/run_data_suggestion.js
@@ -71,7 +71,7 @@ const main = async () => {
 
     await getApps(initialId);
 
-    let priorityList = [1019,1013, 1011, 1000, 1012, 1007, 1004, 1016];
+    let priorityList = [1020,1019,1013, 1011, 1000, 1012, 1007, 1004, 1016];
     let result = [];
     
     for (let priorityId of priorityList) {
