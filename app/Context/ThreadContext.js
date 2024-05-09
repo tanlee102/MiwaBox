f17bc553473cff386aa789fe8f7d1bec9e23ce50
LE QUOC TAN <letanprox@gmail.com>
2024-05-09 15:44:10 +0700

    U

--- app/Context/ThreadContext.js
+++ app/Context/ThreadContext.js
@@ -64,7 +64,7 @@ const ThreadProvider = ({ children }) => {
                     let listUserAddress = [];
                     for(let i = 0; i < msgs.length; i++){
                         remsgs.push({
-                            index: String(i + nanoid() + Math.random().toString(36).substring(2, 15)),
+                            index: String(i + nanoid()),
                             sender: msgs[i].sender,
                             value: msgs[i].value,
                             type: ethers.toNumber(msgs[i].typ),
@@ -164,7 +164,7 @@ const ThreadProvider = ({ children }) => {
                 let listUserAddress = [];
                 for(let i = 0; i < msgs.length; i++){
                     remsgs.push({
-                        index: String(i + nanoid() + Math.random().toString(36).substring(2, 15)),
+                        index: String(i + nanoid()),
                         sender: msgs[i].sender,
                         value: msgs[i].value,
                         type: ethers.toNumber(msgs[i].typ),
