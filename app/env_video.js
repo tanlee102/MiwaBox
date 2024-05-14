cf9f4e182e7b26895a23c5cb246a217f2fae7c92
LE QUOC TAN <letanprox@gmail.com>
2024-05-15 00:25:31 +0700

    Y

--- app/env_video.js
+++ app/env_video.js
@@ -1,6 +1,6 @@
 export const LENGTH_LIST_VIDEO = 21;
 export const url_image_domain = 'https://thumb.miwabox.live/';
 export const url_video_domain = 'https://one.miwabox.live/';
-export const url_video_upload_local = 'http://localhost:3000/file/';
+export const url_video_upload_local = 'http://127.0.0.1:3000/file/';
 export const url_video_upload_worker = 'https://drive.caculus103.workers.dev/';
 export const url_video_worker = 'https://vorker.miwabox.live/';
