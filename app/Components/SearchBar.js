b5a46c5bdb9c22ce3f2f45837c6a84980ece3591
LE QUOC TAN <letanprox@gmail.com>
2024-05-15 23:59:55 +0700

    T

--- app/Components/SearchBar.js
+++ app/Components/SearchBar.js
@@ -46,11 +46,17 @@ const SearchBar = ({closeLeft}) => {
             min="0"
             onKeyDown={(event) => {
               if (event.keyCode === 13) {
-                router.push('/?id='+idSearch);
+                const tmp = idSearch;
+                setIdSearch('')
+                router.push('/?id='+tmp);
               }
             }}
         />
-        <span onClick={() => router.push('/?id='+idSearch)}>
+        <span onClick={() => {
+            const tmp = idSearch;
+            setIdSearch('')
+            router.push('/?id='+tmp);
+        }}>
             <svg viewBox="0 0 24 24" fill="none"><path fillRule="evenodd" clipRule="evenodd" d="M4 11C4 7.13401 7.13401 4 11 4C14.866 4 18 7.13401 18 11C18 14.866 14.866 18 11 18C7.13401 18 4 14.866 4 11ZM11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C13.125 20 15.078 19.2635 16.6177 18.0319L20.2929 21.7071C20.6834 22.0976 21.3166 22.0976 21.7071 21.7071C22.0976 21.3166 22.0976 20.6834 21.7071 20.2929L18.0319 16.6177C19.2635 15.078 20 13.125 20 11C20 6.02944 15.9706 2 11 2Z"/></svg>
         </span>
     </div>
