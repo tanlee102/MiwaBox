4c1a650c96f3ab3272f7e3ca2d86a109a8207f3b
LE QUOC TAN <letanprox@gmail.com>
2024-05-15 23:50:52 +0700

    T

--- app/videos/Component/VideoPage.js
+++ app/videos/Component/VideoPage.js
@@ -9,20 +9,52 @@ const VideoPage = () => {
             <Menu></Menu>
         </div>
         <div id='my-video-page-container'>
-              <video src="http://media.w3.org/2010/05/sintel/trailer.mp4" controls></video>
+              <video src="https://rr5---sn-npoe7nsy.c.drive.google.com/videoplayback?expire=1715799962&ei=at1EZr33GZOBp84Ps6u0-AI&ip=162.158.114.49&id=1af4e0ac44af2f39&itag=37&source=webdrive&requiressl=yes&xpc=EghonaK1InoBAQ==&mh=5J&mm=32%2C26&mn=sn-npoe7nsy%2Csn-3pm76nel&ms=su%2Conr&mv=m&mvi=5&pl=24&ttl=transient&susc=dr&driveid=11QaLdJ2BRKA-hOcOC8syyfPhyuiRgl1-&app=explorer&eaua=MiXkJn4gakU&mime=video/mp4&vprv=1&prv=1&cnr=14&dur=8.126&lmt=1715690330484808&mt=1715788839&fvip=2&subapp=UNKNOWN&txp=0011224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cttl%2Csusc%2Cdriveid%2Capp%2Ceaua%2Cmime%2Cvprv%2Cprv%2Ccnr%2Cdur%2Clmt&sig=AJfQdSswRgIhAMlcKD6cWW0PoEUZ6_XxqT7wHJvypDu-nYUrIFnhOZ8AAiEAzbbLMOuJ2JgN6r0atpIhgZoKP_4q7QWcfcOH_I0xZhg=&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl&lsig=AHWaYeowRgIhAM1DkeON0nOcYJALsOXwr08XH3s3dm9SFSJqmKRjLepfAiEAndwzbvNm5zpt-enqJ2MWnJjNqyN4ZDt_lodYwCIBBLM=" controls></video>
         </div>
 
-        <div id='my-suggested-video-container'>
-            <div className='item-suggested-video'>
-                <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
-            </div>
-            <div className='item-suggested-video'>
-                <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
-            </div>
-            <div className='item-suggested-video'>
-                <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
-            </div>
+
+        <div id='info-video-page-container'>
+        
+          <div id='my-info-video-page-container'>
+            
+          </div>
+
+          <div id='label-suggested-video-container'>
+              Đề xuất
+          </div>
+
+          <div id='my-suggested-video-container'>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+              <div className='item-suggested-video'>
+                  <img src='https://images.unsplash.com/photo-1615220368123-9bb8faf4221b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FtcGxlfGVufDB8fDB8fHww' />
+              </div>
+          </div>
+
         </div>
+
     </div>
   )
 }
