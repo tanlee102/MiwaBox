5e41c41901d86f941dc31f846cbf67c927b32cee
LE QUOC TAN <letanprox@gmail.com>
2024-08-14 20:24:24 +0700

    T

--- app/env.js
+++ app/env.js
@@ -7,7 +7,9 @@ export const env_SMARTCHAIN = {
     TOTAL_CURRENT_ICON: 20,
     NETWORK: {
         chainId: "0x61",
-        rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545","https://bsc-testnet-rpc.publicnode.com","https://bsc-testnet.blockpi.network/v1/rpc/public"],
+        rpcUrls: [
+          // "https://data-seed-prebsc-1-s1.binance.org:8545",
+          "https://bsc-testnet-rpc.publicnode.com","https://bsc-testnet.blockpi.network/v1/rpc/public"],
         chainName: "BNB Smart Chain Testnet",
         nativeCurrency: {
           name: "tBNB",
@@ -26,7 +28,9 @@ export const env_SMARTCHAIN = {
     NETWORKS: [
         {
             chainId: "0x61",
-            rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545","https://bsc-testnet-rpc.publicnode.com","https://bsc-testnet.blockpi.network/v1/rpc/public"],
+            rpcUrls: [
+              // "https://data-seed-prebsc-1-s1.binance.org:8545",
+              "https://bsc-testnet-rpc.publicnode.com","https://bsc-testnet.blockpi.network/v1/rpc/public"],
             chainName: "BNB Smart Chain Testnet",
             nativeCurrency: {
               name: "tBNB",
