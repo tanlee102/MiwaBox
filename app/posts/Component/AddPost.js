d48cf2c60eb217be74f0d94e19c541932de1229f
LE QUOC TAN <letanprox@gmail.com>
2024-09-06 06:50:45 +0700

    T

--- app/posts/Component/AddPost.js
+++ app/posts/Component/AddPost.js
@@ -16,7 +16,7 @@ const AddPost = () => {
   const [onResetTitle, setOnResetTitle] = useState(false)
   const [tags, setTags] = useState(['posts']);
 
-  const {myUser} = useContext(RootLayoutContext);
+  const {myUser, logged} = useContext(RootLayoutContext);
 
   const [displayRotateUpload, setDisplayRotateUpload] = useState(false);
 
@@ -138,14 +138,17 @@ const AddPost = () => {
       </div>
 
       <TagInput tags={tags} setTags={setTags} />
-        <div className='modal-footer'>
-          <div className='group-button-modal non-select' style={{float: 'left'}}>
-              <span onClick={uploadPost}> 
-                  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 30 30"> <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M21,16h-5v5 c0,0.553-0.448,1-1,1s-1-0.447-1-1v-5H9c-0.552,0-1-0.447-1-1s0.448-1,1-1h5V9c0-0.553,0.448-1,1-1s1,0.447,1,1v5h5 c0.552,0,1,0.447,1,1S21.552,16,21,16z"></path></svg>
-                  Create Post
-              </span>
-          </div>
+
+      {logged &&
+      <div className='modal-footer'>
+        <div className='group-button-modal non-select' style={{float: 'left'}}>
+            <span onClick={uploadPost}> 
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 30 30"> <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M21,16h-5v5 c0,0.553-0.448,1-1,1s-1-0.447-1-1v-5H9c-0.552,0-1-0.447-1-1s0.448-1,1-1h5V9c0-0.553,0.448-1,1-1s1,0.447,1,1v5h5 c0.552,0,1,0.447,1,1S21.552,16,21,16z"></path></svg>
+                Create Post
+            </span>
         </div>
+      </div>
+      }
 
     </div>
   );
